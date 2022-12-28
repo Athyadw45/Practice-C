d241849d5d95577f691fe24978bad8439eb674ea
Atharva <atharvawakodikar699@gmail.com>
2022-12-28 23:14:59 +0530

    updated member in structure

--- structure.cpp
+++ structure.cpp
@@ -3,10 +3,10 @@
 
 struct Car
 {
-    char cmpname[50];
+    char companyname[50];
     int manYear;
     int price;
-    bool isNew;
+    // bool isNew;
 };
 
 int main(){
@@ -14,8 +14,8 @@ int main(){
     Car maycar1;
     Car maycar2;
 
-    strcpy(maycar1.cmpname , "BMW");
-    strcpy(maycar2.cmpname , "Audi");
+    strcpy(maycar1.companyname , "BMW");
+    strcpy(maycar2.companyname , "Audi");
 
     maycar1.price =10000;
     maycar2.price = 20000;
@@ -28,11 +28,11 @@ int main(){
 
     
     printf("Printing properties of mycar1\n");
-    printf("maycar1 company name %s\n",maycar1.cmpname);
+    printf("maycar1 company name %s\n",maycar1.companyname);
     printf("maycar1 manufacturing year %d\n",maycar1.manYear); 
 
     printf("Printing properties of mycar2\n");
-    printf("maycar2 company name %s\n",maycar2.cmpname);
+    printf("maycar2 company name %s\n",maycar2.companyname);
     printf("maycar2 price %d\n",maycar2.price); 
     printf("maycar2 isNew %d\n",maycar2.isNew); 
 
@@ -41,7 +41,7 @@ int main(){
     mycarCollection[0] = maycar1;
     mycarCollection[1] = maycar2;
 
-    strcpy(mycarCollection[2].cmpname , "Ferrari");
+    strcpy(mycarCollection[2].companyname , "Ferrari");
     mycarCollection[2].price = 50000;
 
     return 0;
